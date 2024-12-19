3ad0524fe8ab00925903fdf0a788059d49e470e2
JezCam <jeremy@cameron.org.au>
2024-12-19 17:14:09 +1100

    Add game logic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body
-                className={`${geistSans.variable} ${geistMono.variable} font-[family-name:var(--font-geist-sans)] antialiased`}
+                className={`${geistSans.variable} ${geistMono.variable} font-[family-name:var(--font-geist-mono)] antialiased`}
             >
                 {children}
             </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,51 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Letter } from '@/lib/definitions'
 import { useRef, useState } from 'react'
-import { Eye, Forward } from 'lucide-react'
+import { Eye, CornerDownLeft, Repeat2, ArrowRight } from 'lucide-react'
 import { getRandomLetter, getRandomPieceType } from '@/lib/utils'
+import { LETTER_COLOURS } from '@/lib/piece-transforms'
+
+enum State {
+    Guessing = 'guessing',
+    TryingAgain = 'tryingAgain',
+    Revealed = 'revealed',
+    Correct = 'correct',
+    Incorrect = 'incorrect',
+}
 
 export default function Home() {
+    const [state, setState] = useState<State>(State.Guessing)
     const [pieceType, setPieceType] = useState<'corner' | 'edge'>('corner')
     const [letter, setLetter] = useState<Letter>('A')
+    const [input, setInput] = useState<string>()
     const inputRef = useRef<HTMLInputElement>(null)
     const revealRef = useRef<HTMLButtonElement>(null)
     const submitRef = useRef<HTMLButtonElement>(null)
 
+    const handleReveal = () => {
+        setInput(letter)
+        setState(State.Revealed)
+    }
+
     const handleSubmit = () => {
+        if (input === letter) {
+            setState(State.Correct)
+        } else {
+            setState(State.Incorrect)
+            setInput(letter)
+        }
+    }
+
+    const handleTryAgain = () => {
+        setState(State.TryingAgain)
+        setInput('')
+    }
+
+    const handleNext = () => {
+        setState(State.Guessing)
+        setInput('')
         setRandomPiece()
+        inputRef.current?.focus()
     }
 
     const setRandomPiece = () => {
@@ -33,25 +66,79 @@ export default function Home() {
                 <Cube pieceType={pieceType} letter={letter} />
                 {/* Inputs */}
                 <div className="flex gap-3">
-                    <Button
-                        ref={revealRef}
-                        className="h-20 w-20 bg-red-600 [&_svg]:size-8"
-                        variant={'destructive'}
-                    >
-                        <Eye />
-                    </Button>
+                    {state === State.Guessing || state === State.TryingAgain ? (
+                        <Button
+                            onClick={handleReveal}
+                            ref={revealRef}
+                            className="h-20 w-20 rounded-lg bg-red-600 [&_svg]:size-8"
+                            variant={'destructive'}
+                        >
+                            <Eye />
+                        </Button>
+                    ) : (
+                        <Button
+                            onClick={handleTryAgain}
+                            ref={revealRef}
+                            className="h-20 w-20 rounded-lg [&_svg]:size-8"
+                            variant={'default'}
+                        >
+                            <Repeat2 />
+                        </Button>
+                    )}
                     <Input
+                        value={input}
+                        onChange={(e) => {
+                            if (e.target.value.slice(0, 1) != input) {
+                                setInput(
+                                    e.target.value.slice(0, 1).toUpperCase()
+                                )
+                                if (!e.target.value) {
+                                    return
+                                }
+                                submitRef.current?.focus()
+                            }
+                        }}
+                        style={{
+                            outlineColor: LETTER_COLOURS[letter],
+                            outlineOffset: 4,
+                            outlineWidth: 4,
+                            border: {
+                                correct: '1px solid green',
+                                incorrect: '1px solid red',
+                                guessing: '1px solid white',
+                                revealed: '1px solid red',
+                                tryingAgain: '1px solid white',
+                            }[state],
+                            color: {
+                                correct: 'green',
+                                incorrect: 'red',
+                                guessing: 'white',
+                                revealed: 'red',
+                                tryingAgain: 'white',
+                            }[state],
+                        }}
                         ref={inputRef}
-                        className="h-20 w-20 bg-black text-center !text-6xl text-white"
+                        className="h-20 w-20 rounded-lg bg-black text-center !text-6xl text-white ring-offset-black"
                     />
-                    <Button
-                        onClick={handleSubmit}
-                        ref={submitRef}
-                        className="h-20 w-20 [&_svg]:size-8"
-                        variant={'secondary'}
-                    >
-                        <Forward />
-                    </Button>
+                    {state === State.Guessing || state === State.TryingAgain ? (
+                        <Button
+                            onClick={handleSubmit}
+                            ref={submitRef}
+                            className="h-20 w-20 rounded-lg [&_svg]:size-8"
+                            variant={'secondary'}
+                        >
+                            <CornerDownLeft />
+                        </Button>
+                    ) : (
+                        <Button
+                            onClick={handleNext}
+                            ref={submitRef}
+                            className="h-20 w-20 rounded-lg [&_svg]:size-8"
+                            variant={'secondary'}
+                        >
+                            <ArrowRight />
+                        </Button>
+                    )}
                 </div>
             </div>
         </div>
